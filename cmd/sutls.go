62fcdf13a5cfd25a58e46701f475b934af0e859b
cyejing <jing23501@qq.com>
2021-11-18 23:12:50 +0800

    123

--- cmd/sutls.go
+++ cmd/sutls.go
@@ -1,35 +1,65 @@
 package main
 
 import (
-	"github.com/cyejing/shuttle/core"
-	"github.com/cyejing/shuttle/pkg/log"
+	"bufio"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
 )
 
-func main() {
-	log.Info.Println("hello")
-	core.NewEcho(8890)
-	//server, err := net.ListenTCP("tcp", &net.TCPAddr{Port: 8890})
-	//if err != nil {
-	//	return
-	//}
-	//for {
-	//	conn, err := server.AcceptTCP()
-	//	if err != nil {
-	//		return
-	//	}
-	//	go handler(conn)
-	//}
+//
+//func main() {
+//	log.Info.Println("hello")
+//	//core.NewEcho(8890)
+//	server, err := net.ListenTCP("tcp", &net.TCPAddr{Port: 8890})
+//	if err != nil {
+//		return
+//	}
+//	for {
+//		conn, err := server.AcceptTCP()
+//		if err != nil {
+//			return
+//		}
+//		go handleConn(conn)
+//	}
+//}
+//
+func handleConn(conn net.Conn) {
+	bs := make([]byte, 56)
+	conn.Read(bs)
+	fmt.Println(string(bs))
+	fmt.Println("================")
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		//conn.Write(scanner.Bytes())
+		//conn.Write( []byte("\n"))
+	}
+
 }
 
-//
-//func handler(conn *net.TCPConn) {
-//	scanner := bufio.NewScanner(conn)
-//	for scanner.Scan() {
-//		fmt.Println(scanner.Text())
-//		//fmt.Println([]byte(scanner.Text()))
-//		//fmt.Println(scanner.Bytes())
-//		conn.Write(scanner.Bytes())
-//		//conn.Write( []byte("\n"))
-//	}
-//
-//}
+func main() {
+	cert, err := tls.LoadX509KeyPair(
+		"/Users/cyejing/Project/born/shuttle/mini.cyejing.cn_chain.crt",
+		"/Users/cyejing/Project/born/shuttle/mini.cyejing.cn_key.key")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	config := &tls.Config{Certificates: []tls.Certificate{cert}}
+	ln, err := tls.Listen("tcp", ":8890", config)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
---
+++ cmd/web.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	http.ListenAndServeTLS(":8890",
+		"/Users/cyejing/Project/born/shuttle/mini.cyejing.cn_chain.crt",
+		"/Users/cyejing/Project/born/shuttle/mini.cyejing.cn_key.key",
+		nil)
+}
+
+//!+handler
+// handler echoes the HTTP request.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+//!-handler
